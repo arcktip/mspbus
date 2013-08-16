ed977b33dd7b38d2cc1fef13a77de586ab74c95a
Coffeeman <louis@goessling.com>
2013-08-16 14:12:09 -0500

    Changed response architecturee

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,27 +1,24 @@
 def gen_twillio_data(stopid)
-	  stops=Stop.get_stop_by_id({:id=>stopid})
-      if stops.results.empty?
-        return "Couldn't find stop."
-      end
+  stops=Stop.get_stop_by_id({:id=>stopid})
+  if stops.results.empty?
+    return "Couldn't find stop."
+  end
 
-      response = HTTParty.get("http://svc.metrotransit.org/NexTrip/#{stopid}?format=json")
-      puts "http://svc.metrotransit.org/NexTrip/#{stopid}?format=json"
-      if not response.code==200
-        return "An error occoured! Sorry."
-      end
-      stopfound=true
-      puts response.body, response.code, response.message, response.headers.inspect
-      arrivals = JSON.parse response.body
+  response = HTTParty.get("http://svc.metrotransit.org/NexTrip/#{stopid}?format=json")
+  if not response.code==200
+    return "An error occoured! Sorry."
+  end
+  stopfound=true
 
-      smess=""
-      response.each do |item|
-        smess+=item['RouteDirection'][0]+item['Route']+item['Terminal']+" "+item['DepartureText']+", "
-      end
-      smess=smess[0..159]
-      if smess[-2]==','
-        smess=smess[0..-3]
-      end
-      return smess
+  smess=""
+  response.each do |item|
+    smess+=item['RouteDirection'][0]+item['Route']+item['Terminal']+" "+item['DepartureText']+", "
+  end
+  smess=smess[0..159]
+  if smess[-2]==','
+    smess=smess[0..-3]
+  end
+  return smess
 end
 
 class HomeController < ApplicationController
@@ -60,7 +57,7 @@ class HomeController < ApplicationController
   end
 
   def sms
-    render :layout => false
+    #render :layout => false
     
     #  Twilio POST request parameters
     #  Parameter	Description
@@ -72,7 +69,11 @@ class HomeController < ApplicationController
     if not params[:Body].index(' ')
       @stopid=params[:Body]
       @smess=gen_twillio_data(@stopid)
-      
+      puts '===='
+      puts @smess
+      respond_to do |format|
+      	format.all { render :text => "<Response><Sms>#{@smess}</Sms></Response>" }
+      end
     else
       puts "Space"
     end
