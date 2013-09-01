5901e5b22b3ed1b8a460d692688a67b7526a8064
Richard Barnes <rbarnes@umn.edu>
2013-09-01 17:39:32 -0500

    Eliminated extraneous code from config.js

--- app/assets/javascripts/config.js
+++ app/assets/javascripts/config.js
@@ -5,10 +5,4 @@ var config = {
   //Bounds of the map used to determine if geolocation is outside bounds
   //and to bias the address geocoding look-up
   bounds: {west:-94.01, east:-92.73, north:45.42, south:44.47},
-
-  //Assuming a system with only one transit provider, this formats the API
-  //call to get the stop information based on the stopid
-  //TODO: The API call functionality needs to be abstracted away from the
-  //particulars of a given system
-  api_url: function(stopid) { return 'http://svc.metrotransit.org/NexTrip/' + stopid + '?callback=?&format=json'; }
 };
