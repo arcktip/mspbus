6ecce2f5722bf4deb2ef667c13468c4910f8bfe4
Richard Barnes <rbarnes@umn.edu>
2013-09-01 21:28:24 -0500

    Remembers which tab was being used

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -26,7 +26,6 @@ var HomeView = Backbone.View.extend({
     // We are on a small screen, should determine view to show.
     if ( matchMedia('only screen and (max-width: 767px)').matches ) {
       HomeView.mobile=true; //TODO: Is this the right place to attach this?
-      this.determine_view();
     } else {
       HomeView.mobile=false; //TODO: Is this the right place to attach this?
     }
@@ -35,6 +34,8 @@ var HomeView = Backbone.View.extend({
     //     this.map_view.ran = true;
     //   }
     // }
+
+    this.determine_view();
   },
 
   determine_view: function() {
