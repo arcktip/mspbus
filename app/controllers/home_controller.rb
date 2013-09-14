48f3230f23aa0aeb1bda5abfdc78fb6239859e0d
Coffeeman <louis@goessling.com>
2013-09-14 16:24:55 -0500

    Replaced the temporary TWIML with the correct TWIML.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -81,8 +81,7 @@ class HomeController < ApplicationController
   
   def voice
     respond_to do |format|
-      #format.all { render :text => '<Response><Gather timeout="10" finishOnKey="*" action="http://omgtransit.org/voice_respond"><Say>Please enter the stop I D, then press star.</Say></Gather></Response>' }
-      format.all { render :text => '<Response><Say>This feature coming soon.</Say></Response>' }
+      format.all { render :text => '<Response><Gather timeout="10" finishOnKey="*" action="http://omgtransit.org/voice_respond"><Say>Please enter the stop I D, then press star.</Say></Gather></Response>' }
     end
   end
   
