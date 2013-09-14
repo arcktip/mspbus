793bd52aad4972a1649c8015cf042d3237ae0120
Richard Barnes <rbarnes@umn.edu>
2013-09-14 17:04:16 -0500

    Committed redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :redirect_if_old
+
+  protected
+
+  #TODO: Remove this after a while
+  def redirect_if_old
+    if request.host != 'omgtransit.com'
+      redirect_to "http://omgtransit.com#{request.path}", :status => :moved_permanently 
+    end
+  end
 end
