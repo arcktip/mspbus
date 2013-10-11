002f52c0ca8bf6d822d938e561f96a2a5700db41
Richard Barnes <rbarnes@umn.edu>
2013-10-11 16:45:09 -0500

    Eliminated redirect code from application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_filter :redirect_if_old
-
-  protected
-
-  #TODO: Remove this after a while
-  def redirect_if_old
-    if request.host != 'omgtransit.com' and request.host != 'localhost'
-      redirect_to "http://omgtransit.com#{request.path}", :status => :moved_permanently 
-    end
-  end
 end
