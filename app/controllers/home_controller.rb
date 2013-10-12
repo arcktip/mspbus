16e342d24b9e8fbded81dc4cb0f739161e86062f
Richard Barnes <rbarnes@umn.edu>
2013-10-12 17:06:50 -0500

    Commented the code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,20 @@ class HomeController < ApplicationController
   end
 
   def table
+    #First we'll see if there are any stops within 10 miles of the user
     @in_bounds=true
     params[:radius] = 10
     bounds_stops = Stop.search(params)
+
+    #If there were no stops within 10 miles, probably the user is not part
+    #of our service area. Make a note of this and give them a default location.
     if bounds_stops.results.empty?
       @in_bounds=false
       params[:lat]= 44.980522382993826
       params[:lon]=-93.27006340026855
     end
 
+    #Use the user's location and find all stops within 1 mile of it
     params[:radius] = 1
     @stops = Stop.search(params)
     @lat=params[:lat]
