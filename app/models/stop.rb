82a5a6fabc50cae223dda6033b0db84ee685ec14
Richard Barnes <rbarnes@umn.edu>
2013-11-16 18:20:51 -0600

    Made stop_region and stop_postcode accessible attributes of the Stop model in stop.rb

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -5,7 +5,7 @@ class Stop < ActiveRecord::Base
 
   self.primary_key = :id
 
-  attr_accessible :id, :stop_id, :source_id, :stop_name, :stop_desc, :stop_lat, :stop_lon, :stop_city, :stop_street, :url, :stop_type, :stop_code, :zone_id, :stop_url
+  attr_accessible :id, :stop_id, :source_id, :stop_name, :stop_desc, :stop_lat, :stop_lon, :stop_city, :stop_street, :url, :stop_type, :stop_code, :zone_id, :stop_url, :stop_region, :stop_postcode
   
   belongs_to :source
   has_many :favorites
